e432db6c23cc2db63794ccad3d41d76395fee128
Ahmed-SM <f_22w3@hotmail.com>
2020-02-04 12:55:32 +0400

    Validation

--- js.js
+++ js.js
@@ -14,7 +14,18 @@ for (let index = n; index >= 1950; index--) {
   option.text = index;
   selectYear.add(option);
 }
-
+$("input").prop('required',true);
+$(function () {
+  var inputs = document.getElementsByTagName("INPUT");
+  for (var i = 0; i < inputs.length; i++) {
+      inputs[i].oninvalid = function (e) {
+          e.target.setCustomValidity("");
+          if (!e.target.validity.valid) {
+              e.target.setCustomValidity(e.target.getAttribute("data-error"));
+          }
+      };
+  }
+});
 // $(document).ready(function () {
 //   $("form").change(function () {
 //     console.log(
@@ -25,7 +36,7 @@ for (let index = n; index >= 1950; index--) {
 //   });
 // });
 
-document.getElementById("submit-btn").addEventListener("click", function (e) {
+document.getElementById("submit-btn").addEventListener("submit", function (e) {
   e.preventDefault();
   let list = [];
   let br = 0;
@@ -70,3 +81,79 @@ function TodayDate() {
   return today = mm + '/' + dd + '/' + yyyy;
 
 }
+
+// document.getElementById("extra-btn").addEventListener("click", function () {
+//   var node = document.createElement("div");
+//   var clone = document.getElementById("selectors-section").cloneNode(true);
+//   node.appendChild(clone);
+//   document.getElementById("data-section").appendChild(node);
+// });
+
+// document.getElementById("request-date").value = TodayDate();
+// var selectYear = document.getElementById("select-year");
+// var d = new Date();
+// var n = d.getFullYear();
+// for (let index = n; index >= 1950; index--) {
+//   var option = document.createElement("option");
+//   option.text = index;
+//   selectYear.add(option);
+// }
+
+// // $(document).ready(function () {
+// //   $("form").change(function () {
+// //     console.log(
+// //       $(this)
+// //       .closest("form")
+// //       .serialize()
+// //     );
+// //   });
+// // });
+
+// document.getElementById("submit-btn").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   let list = [];
+//   let br = 0;
+//   $("select").each(function () {
+//     if (br == 3) {
+//       list.push("      ");
+//       br = 0;
+//     }
+//     list.push($(this).val())
+//     br++;
+//   })
+//   var body = `المقدم: ${$("#sname").val()}` + '\r\n' + `التاريخ: ${TodayDate()}` +
+//     '\r\n' + `الهاتف: ${$("#sphone").val()}` + '\r\n' + `الطالب: ${$("#student-name").val()}` +
+//     '\r\n' + `المدرسة: ${$("#school-name").val()}` +
+//     '\r\n' + `الطلب: ${list}`;
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "9597011@gmail.com",
+//     Password: "A18953960FDE630C1CD2345CD4EEDFD47455",
+//     To: "chcrak@gmail.com",
+//     From: "chcrak@gmail.com",
+//     Subject: `${$("#student-name").val()}طلب تسلسل دراسي لـ`,
+//     Body: body
+//   }).then((message) => {
+//     if (message == "OK") {
+//       $("#main").addClass("blur");
+//       $("#main2").removeClass("hide");
+//       $("#overlay").addClass("disable")
+//       $("body").css("overflow", "hidden");
+//     } else {
+//       $("#main").addClass("blur");
+//       $("#main4").removeClass("hide");
+//       $("#overlay").addClass("disable")
+//       $("body").css("overflow", "hidden");
+//       alert(message)
+//     }
+//   });
+// });
+
+// function TodayDate() {
+//   var today = new Date();
+//   var dd = String(today.getDate()).padStart(2, '0');
+//   var mm = String(today.getMonth() + 1).padStart(2, '0');
+//   var yyyy = today.getFullYear();
+//   return today = mm + '/' + dd + '/' + yyyy;
+
+// }
